632309bd0c454c202e326ca2c8683c7d6c9860df
Prajuk Nusbaum <prajuk.nusbaum@dps.texas.gov>
2023-01-05 20:10:42 -0600

    Interesting Sum function

---
+++ Methods/MathShortcuts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal static class MathShortcuts
+    {
+        public static void Add(double x, double y)
+        {
+            Console.WriteLine($"The value of {x} + {y} = {x+y}" );
+        }
+        public static void AddAll(double[] values)
+        {
+            double result = 0;
+            foreach (double value in values)
+            {
+                result += value;
+            }
+            Console.WriteLine($"The total is {result}");
+        }
+    }
+}
--- Methods/Program.cs
+++ Methods/Program.cs
@@ -5,13 +5,27 @@
 //
 
 using Methods;
-
+using System.ComponentModel;
+/*
 ConsoleMessages.SayHi();
 for (int i = 0; i < 5; i++)
 {
-    ConsoleMessages.SayHi(i);
-    ConsoleMessages.SayHi();
-    ConsoleMessages.SayHi("Have a nice Day");
+ConsoleMessages.SayHi(i);
+ConsoleMessages.SayHi();
+ConsoleMessages.SayHi("Have a nice Day");
 }
+*/
+
+ConsoleMessages.SayHi("Pro");
+
+MathShortcuts.Add(5, 3);
+
+double[] vals = new double[] { 5, 6, 7, 8, 10, 12 };
+
+
+
+MathShortcuts.AddAll(vals);
+ConsoleMessages.SayHi("Good-Bye");
+ConsoleMessages.SayHi($"{vals.Sum().ToString()}");
 
 
